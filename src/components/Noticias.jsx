f6a3e8a0293e4b89290aacde8f237e4266d5b74c
javiergraphico <javieralexander38@hotmail.com>
2023-10-17 00:48:43 -0400

    svg final final

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -19,7 +19,10 @@ const Noticias = () => {
               <hr className='noticias_hr'></hr>
               <p className='noticias_p_date'>May 29, 2019</p>
             </div>
-            <BiPlusCircle className='noticias_icon_plus' />
+            <svg className='noticias_icon_plus' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
           </div>
 
           <div className='noticias_card'>
@@ -33,7 +36,10 @@ const Noticias = () => {
               <hr className='noticias_hr'></hr>
               <p className='noticias_p_date'>May 29, 2019</p>
             </div>
-            <BiPlusCircle className='noticias_icon_plus' />
+            <svg className='noticias_icon_plus' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
           </div>
 
           <div className='noticias_card'>
@@ -47,7 +53,10 @@ const Noticias = () => {
               <hr className='noticias_hr'></hr>
               <p className='noticias_p_date'>May 29, 2019</p>
             </div>
-            <BiPlusCircle className='noticias_icon_plus' />
+            <svg className='noticias_icon_plus' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
           </div>
         </div>
       </div>
