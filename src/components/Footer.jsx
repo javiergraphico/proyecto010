96dc6946966b5eed3401ba8394eb08045b800190
javiergraphico <javieralexander38@hotmail.com>
2023-10-17 12:38:17 -0400

    cambio en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <ScrollToTop smooth top="4000" width="20" height="20" style={{ marginLeft: "10px" }} />
+      <ScrollToTop smooth top="4000" width="20" height="20" style={{ position: 'absolute', right: '20px', bottom: '20px'}} />
       <div className='contenedor'>
         <div className='footer_contenedor'>
           <div className='footer_conten_logo'>
