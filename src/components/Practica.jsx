f711f15e08153420555307ce8fe844669e556380
javiergraphico <javieralexander38@hotmail.com>
2023-10-17 00:14:50 -0400

    cambio de svg

--- src/components/Practica.jsx
+++ src/components/Practica.jsx
@@ -14,7 +14,9 @@ const Practica = () => {
         <div className='practica_contenedor'>
           <div className='practica_card'>
               <div className='practica_content_icons'>
-                <CiUser className='practica_item_icon' />
+                <svg className='practica_item_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
               </div>
               <h3 className='practica_h3'>Family Law</h3>
               <p className='practica_p'>We aim to support families in resolving their own disputes. We ensure professional consultancy and result.</p>
@@ -22,7 +24,10 @@ const Practica = () => {
 
           <div className='practica_card'>
               <div className='practica_content_icons'>
-                <PiBagSimple className='practica_item_icon' />
+                <svg className='practica_item_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+
               </div>
               <h3 className='practica_h3'>Business Law</h3>
               <p className='practica_p'>We aim to support families in resolving their own disputes. We ensure professional consultancy and result.</p>
@@ -30,7 +35,10 @@ const Practica = () => {
 
           <div className='practica_card'>
               <div className='practica_content_icons'>
-                <PiBookOpenLight className='practica_item_icon' />
+                <svg className='practica_item_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                </svg>
+
               </div>
               <h3 className='practica_h3'>Civil Litigation</h3>
               <p className='practica_p'>We aim to support families in resolving their own disputes. We ensure professional consultancy and result.</p>
@@ -46,7 +54,10 @@ const Practica = () => {
 
           <div className='practica_card'>
               <div className='practica_content_icons'>
-                <HiOutlineHome className='practica_item_icon' />
+                <svg className='practica_item_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+
               </div>
               <h3 className='practica_h3'>Land & Assets</h3>
               <p className='practica_p'>We aim to support families in resolving their own disputes. We ensure professional consultancy and result.</p>
@@ -54,7 +65,10 @@ const Practica = () => {
 
           <div className='practica_card'>
               <div className='practica_content_icons'>
-                <TbMoneybag className='practica_item_icon' />
+                <svg className='practica_item_icon' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                </svg>
+
               </div>
               <h3 className='practica_h3'>Finance Law</h3>
               <p className='practica_p'>We aim to support families in resolving their own disputes. We ensure professional consultancy and result.</p>
