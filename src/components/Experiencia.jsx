f7e597df01ef7206c4e9caf7e265db27ac2882d3
javiergraphico <javieralexander38@hotmail.com>
2023-10-18 18:19:35 -0400

    cambio h3

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -9,39 +9,39 @@ const Experiencia = () => {
         <p className='experiencia_parafo'>We’re proud that our law firm offers top-notch legal services for a nationwide affordable pricing! With us you’ll never feel like the lawyers are just robbers in suits, besides, we win 98% of all cases. So with us, your chances of winning are as high as they possibly can be!</p>
         <div className='contenedor_experiencia'>
           <div className='experiencia_card'>
-            <h2 className='experiencia_h3'>
+            <h3 className='experiencia_h3'>
               1998
-            </h2>
-            <h4 className='experiencia_p'>
+            </h3>
+            <h5 className='experiencia_p'>
               COMPANY <br /> ESTABLISHED
-            </h4>
+            </h5>
           </div>
 
           <div className='experiencia_card'>
-            <h2 className='experiencia_h3'>
+            <h3 className='experiencia_h3'>
               547
-            </h2>
-            <h4 className='experiencia_p'>
+            </h3>
+            <h5 className='experiencia_p'>
               CASES <br /> WON
-            </h4>
+            </h5>
           </div>
 
           <div className='experiencia_card'>
-            <h2 className='experiencia_h3'>
+            <h3 className='experiencia_h3'>
               45+
-            </h2>
-            <h4 className='experiencia_p'>
+            </h3>
+            <h5 className='experiencia_p'>
               BUSINESS <br /> PARTNERS
-            </h4>
+            </h5>
           </div>
 
           <div className='experiencia_card'>
-            <h2 className='experiencia_h3'>
+            <h3 className='experiencia_h3'>
               1500
-            </h2>
-            <h4 className='experiencia_p'>
+            </h3>
+            <h5 className='experiencia_p'>
               TRUSTING <br /> CLIENTS
-            </h4>
+            </h5>
           </div>
         </div>
       </article>
